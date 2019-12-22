b04592d80cf31148a4355e768825276515d466d7
olivier debauche <olivier.debauche@umons.ac.be>
2019-12-22 09:55:14 +0100

    Initial commit

---
+++ lib/SensorSemantic.dart
@@ -0,0 +1,479 @@
+import 'package:flutter/material.dart';
+
+class SensorSemantic {
+  // Variables declaration
+  String Id;
+  String name;
+  String type;
+  String vendorName;
+  String version;
+  String power;
+  String resolution;
+  String maxRange;
+  String maxDelay;
+  String minDelay;
+  String reportingMode;
+  String isWakeup;
+  String isDynamic;
+  String isAdditionalInfoSupported;
+  String isDirectChannelTypeSupported;
+  String highestDirectReportRateValue;
+  String fifoMaxEventCount;
+  String fifoReservedEventCount;
+
+  String _getTypeToName(String type) {
+    return <String, String>{
+      '1': 'Accelerometer',
+      '2': 'Magnetic Field',
+      '3': 'Orientation',
+      '4': 'Gyroscope',
+      '5': 'Ambient Light',
+      '6': 'Atmospheric Pressure',
+      '8': 'Proximity',
+      '9': 'Gravity',
+      '10': 'Linear Acceleration',
+      '11': 'Rotation Vector',
+      '12': 'Relative Humidity',
+      '13': 'Ambient Temperature',
+      '14': 'Uncalibred Magnetic Filed',
+      '15': 'Game Rotation Vector',
+      '16': 'Uncalibred Gyroscope',
+      '17': 'Signifiant Motion',
+      '19': 'Step Counter',
+      '20': 'Geo Magnetic Rotation Vector',
+      '21': 'Heart Rate',
+      '28': 'Pose 6DOF',
+      '29': 'Stationary Detection',
+      '30': 'Motion Detection',
+      '31': 'Heart Beat',
+      '34': 'Low Latency Off Body Detection',
+      '35': 'Uncalibred Accelerometer'
+    } [type];
+  }
+
+  // Constructor
+  SensorSemantic(
+      this.Id,
+      this.name,
+      this.type,
+      this.vendorName,
+      this.version,
+      this.power,
+      this.resolution,
+      this.maxRange,
+      this.maxDelay,
+      this.minDelay,
+      this.reportingMode,
+      this.isWakeup,
+      this.isDynamic,
+      this.isAdditionalInfoSupported,
+      this.isDirectChannelTypeSupported,
+      this.highestDirectReportRateValue,
+      this.fifoMaxEventCount,
+      this.fifoReservedEventCount) {
+    type = '${_getTypeToName(type)} ($type)';
+  }
+}
+
+// Type 1 - Accelerometer
+class Accelerometer {
+  // Variables declaration
+  SensorSemantic sensor;
+  String x;
+  String y;
+  String z;
+  // Constructor
+  Accelerometer (
+    this.sensor,
+    this.x,
+    this.y,
+    this.z
+  );
+}
+
+// Type 2 - Magnetic Field
+class MagneticField {
+  // Variables declaration
+  SensorSemantic sensor;
+  String x;
+  String y;
+  String z;
+  // Constructor
+  MagneticField(
+    this.sensor,
+    this.x,
+    this.y,
+    this.z
+  );
+}
+
+// Type 3 - Orientation
+class OrientationSensor {
+  // Variables declaration
+  SensorSemantic sensor;
+  String azimuth;
+  String pitch;
+  String roll;
+  // Constructor
+  OrientationSensor(
+    this.azimuth,
+    this.pitch,
+    this.roll
+  );
+}
+
+// Type 4 - Gyroscope
+class Gyroscope {
+  // Variables declaration
+  SensorSemantic sensor;
+  String x;
+  String y;
+  String z;
+  // Constructor
+  Gyroscope(
+    this.sensor,
+    this.x,
+    this.y,
+    this.z
+  );
+}
+
+// Type 5 - Ambient Light
+class AmbientLight {
+  // Variable declaration
+  SensorSemantic sensor;
+  String level;
+  // Constructor
+  AmbientLight(
+    this.sensor,
+    this.level
+  );
+}
+
+// Type 6 - Atmospheric Pressure
+class AtmosphericPressure {
+  // Variable declaration
+  SensorSemantic sensor;
+  String pressure;
+  // Constructor
+  AtmosphericPressure(
+    this.sensor,
+    this.pressure
+  );
+}
+
+// Type 8 - Proximity
+class Proximity {
+  // Variable declaration
+  SensorSemantic sensor;
+  String distance;
+  // Constructor
+  Proximity(
+    this.sensor,
+    this.distance
+  );
+}
+
+// Type 9 - Gravity
+class Gravity {
+  // Variables declaration
+  SensorSemantic sensor;
+  String x;
+  String y;
+  String z;
+  // Constructor
+  Gravity(
+    this.sensor,
+    this.x,
+    this.y,
+    this.z
+  );
+}
+
+// Type 10 - LinearAcceleration
+class LinearAcceleration {
+  // Variables declaration
+  SensorSemantic sensor;
+  String x;
+  String y;
+  String z;
+  // Constructor
+  LinearAcceleration(
+    this.sensor,
+    this.x,
+    this.y,
+    this.z
+  );
+}
+
+// Type 11 - RoationVector
+class RotationVector {
+  // Variables declaration
+  SensorSemantic sensor;
+  String x;
+  String y;
+  String z;
+  String someVal;
+  String estimatedHeadingAccuracy;
+  // Constructor
+  RotationVector(
+    this.sensor,
+    this.x,
+    this.y,
+    this.z,
+    this.someVal,
+    this.estimatedHeadingAccuracy
+  );
+}
+
+// Type 12 - Relative Humidity
+class RelativeHumidity {
+  // Variable Declaration
+  SensorSemantic sensor;
+  String humidity;
+  // Constructor
+  RelativeHumidity (
+    this.sensor,
+    this.humidity
+  );
+}
+
+// Type 13 - AmbientTemperature
+class AmbientTemperature {
+  // Variables declaration
+  SensorSemantic sensor;
+  String temperature;
+  // Constructor
+  AmbientTemperature(
+    this.sensor,
+    this.temperature
+  );
+}
+
+// Type 14 - Uncalibrated Magnetic Field
+class UncalibratedMagneticField {
+  // Variables declaration
+  SensorSemantic sensor;
+  String xUncalibrated;
+  String yUncalibrated;
+  String zUncalibrated;
+  String estimatedXBias;
+  String estimatedYBias;
+  String estimatedZBias;
+  // Constructor
+  UncalibratedMagneticField(
+    this.sensor,
+    this.xUncalibrated,
+    this.yUncalibrated,
+    this.zUncalibrated,
+    this.estimatedXBias,
+    this.estimatedYBias,
+    this.estimatedZBias
+  );
+}
+
+// Type 15 - GameRotationVector
+class GameRotationVector {
+  // Variables declaration
+  SensorSemantic sensor;
+  String x;
+  String y;
+  String z;
+  String someVal;
+  String estimatedHeadingAccuracy;
+  // Constructor
+  GameRotationVector(
+    this.x,
+    this.y,
+    this.z,
+    this.someVal,
+    this.estimatedHeadingAccuracy
+  );
+}
+
+// Type 16 - UncalibratedGyroscope
+class UncalibratedGyroscope {
+  // Variables declaration
+  SensorSemantic sensor;
+  String angularSpeedAroundX;
+  String angularSpeedAroundY;
+  String angularSpeedAroundZ;
+  String estimatedDriftAroundX;
+  String estimatedDriftAroundY;
+  String estimatedDriftAroundZ;
+  // Constructor
+  UncalibratedGyroscope(
+    this.sensor,
+    this.angularSpeedAroundX,
+    this.angularSpeedAroundY,
+    this.angularSpeedAroundZ,
+    this.estimatedDriftAroundX,
+    this.estimatedDriftAroundY,
+    this.estimatedDriftAroundZ
+  );
+}
+
+// Type 17 - Signifiant Motion
+class SignifiantMotion {
+  // Variable déclaration
+  SensorSemantic sensor;
+  String isInMotion;
+  // Constructor
+  SignifiantMotion(
+    this.sensor,
+    this.isInMotion
+  );
+}
+
+// Type 19 - Step Counter
+class StepCounter {
+  // Variables déclaration
+  SensorSemantic sensor;
+  String step;
+  StepCounter(
+    this.sensor,
+    this.step
+  );
+}
+
+// Type 20 - GeoMagneticRotationVector
+class GeoMagneticRotationVector {
+  // Variables declaration
+  SensorSemantic sensor;
+  String x;
+  String y;
+  String z;
+  String someVal;
+  String estimatedHeadingAccuracy;
+  // Constructor
+  GeoMagneticRotationVector(
+    this.sensor,
+    this.x,
+    this.y,
+    this.z,
+    this.someVal,
+    this.estimatedHeadingAccuracy
+  );
+}
+
+//Type 21 - Heart Rate
+class HeartRate {
+  // Variable declaration
+  SensorSemantic sensor;
+  String rate;
+  //Constructor
+  HeartRate(
+    this.sensor,
+    this.rate
+  );
+}
+
+// Type 28 - Pose 6DOF
+class pose6DOF {
+  // Variables Declaration
+  SensorSemantic sensor;
+  String xSinTheta;
+  String ySinTheta;
+  String zSinTheta;
+  String cosTheta;
+  String xTranslation;
+  String yTranslation;
+  String zTranslation;
+  String xDeltaQuaternion;
+  String yDeltaQuaternion;
+  String zDeltaQuaternion;
+  String cosDeltaQuaternion;
+  String xDeltaTranslation;
+  String yDeltaTranslation;
+  String zDeltaTranslation;
+  String sequenceNumber;
+  // Constructor
+  pose6DOF(
+    this.sensor,
+    this.xSinTheta,
+    this.ySinTheta,
+    this.zSinTheta,
+    this.cosTheta,
+    this.xTranslation,
+    this.yTranslation,
+    this.zTranslation,
+    this.xDeltaQuaternion,
+    this.yDeltaQuaternion,
+    this.zDeltaQuaternion,
+    this.cosDeltaQuaternion,
+    this.xDeltaTranslation,
+    this.yDeltaTranslation,
+    this.zDeltaTranslation,
+    this.sequenceNumber
+  );
+}
+
+// Type 29 - Stationary Detection
+class StationaryDetection {
+  // Variable declaration
+  SensorSemantic sensor;
+  String isImmobile;
+  // Constructor
+  StationaryDetection(
+    this.sensor,
+    this.isImmobile
+  );
+}
+
+// Type 30 - Motion Detection
+class MotionDetection {
+  // Variables declaration
+  SensorSemantic sensor;
+  String isInMotion;
+  // Constructor
+  MotionDetection(
+    this.sensor,
+    this.isInMotion
+  );
+}
+
+// Type 31 - Heart Beat
+class HeartBeat {
+  // Variables declaration
+  SensorSemantic sensor;
+  String confidence;
+  // Constructor
+  HeartBeat(
+    this.sensor,
+    this.confidence
+  );
+}
+
+// Type 34 - Low Latency Off Body Detection
+class LowLatencyOffBodyDetection {
+  // Variables declaration
+  SensorSemantic sensor;
+  String offBodySate;
+  // Constructor
+  LowLatencyOffBodyDetection (
+    this.sensor,
+    this.offBodySate
+  );
+}
+
+// Type 35 - UncalibratedAccelerometer
+class UncalibratedAccelerometer {
+  // Variable declaration
+  SensorSemantic sensor;
+  String xUncalibrated;
+  String yUncalibrated;
+  String zUncalibrated;
+  String estimatedXBias;
+  String estimatedYBias;
+  String estimatedZBias;
+  // Constructor
+  UncalibratedAccelerometer(
+    this.sensor,
+    this.xUncalibrated,
+    this.yUncalibrated,
+    this.zUncalibrated,
+    this.estimatedXBias,
+    this.estimatedYBias,
+    this.estimatedZBias
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,387 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'SensorSemantic.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
+
+void main() => runApp(CowBehaviorMain());
+
+class CowBehaviorMain extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cow Behavior App',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+SensorSemantic getInstanceOfSensorSemantic(Map<String, String> data) {
+  return SensorSemantic (
+    data['Id'],
+    data['name'],
+    data['type'],
+    data['vendorName'],
+    data['version'],
+    data['power'],
+    data['resolution'],
+    data['maxRange'],
+    data['maxDelay'],
+    data['minDelay'],
+    data['reportingMode'],
+    data['isWakeup'],
+    data['isDynamic'],
+    data['isAdditionalInfoSupported'],
+    data['isDirectChannelTypeSupported'],
+    data['highestDirectReportRateValue'],
+    data['fifoMaxEventCount'],
+    data['fifoReservedEventCount']
+  );
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static String _methodChannelName = 'eu.smartappli.cowbehavior.androidMethodChannel';
+  static String _eventChannelName = 'eu.smartappli.cowbehavior.androidEventChannel';
+  static MethodChannel _methodChannel;
+  static EventChannel _eventChannel;
+
+  List<Accelerometer> _listAccelerometer = [];                            // Type 1
+  List<MagneticField> _listMagnetometer = [];                             // Type 2
+  List<OrientationSensor> _listOrientationSensor = [];                    // Type 3
+  List<Gyroscope> _listGyroscope = [];                                    // Type 4
+  List<AmbientLight> _listAmbientLight = [];                              // Type 5
+  List<AtmosphericPressure> _listAtmosphericPressure = [];                // Type 6
+  List<Proximity> _listProximity = [];                                    // Type 8
+  List<Gravity> _listGravity = [];                                        // Type 9
+  List<LinearAcceleration> _listLinearAcceleration = [];                  // Type 10
+  List<RotationVector> _listRotationVector = [];                          // Type 11
+  List<RelativeHumidity> _listRelativeHumidity = [];                      // Type 12
+  List<AmbientTemperature> _listAmbientTemperature = [];                  // Type 13
+  List<UncalibratedMagneticField> _listUncalibratedMagneticField = [];    // Type 14
+  List<GameRotationVector> _listGameRotationVector = [];                  // Type 15
+  List<UncalibratedGyroscope> _listUncalibratedGyroscope = [];            // Type 16
+  List<SignifiantMotion> _listSignifiantMotion = [];                      // Type 17
+  List<StepCounter> _listStepCounter = [];                                // Type 19
+  List<GeoMagneticRotationVector> _listGeoMagneticRotationVector = [];    // Type 20
+  List<HeartRate> _listHeartRate = [];                                    // Type 21
+  List<pose6DOF> _listpose6DOF = [];                                      // Type 28
+  List<StationaryDetection> _listStationaryDetection = [];                // Type 29
+  List<MotionDetection> _listMotionDetection = [];                        // Type 30
+  List<HeartBeat> _listHeartBeat = [];                                    // Type 31
+  List<LowLatencyOffBodyDetection> _listLowLatencyOffBodyDetection = [];  // Type 34
+
+  Future<void> getSensorsList() async {
+    Map<String, List<dynamic>> sensorCount;
+    try {
+      Map<dynamic, dynamic> tmp =  await _methodChannel.invokeMethod('getSensorList');
+      sensorCount = Map<String, List<dynamic>>.from(tmp);
+      sensorCount.forEach((String key, List<dynamic> value) {
+        switch(key) {
+          case '1': // Accelerometer
+            if (value.length > 0) {
+              value.forEach((dynamic element) {
+                _listAccelerometer.add(Accelerometer(
+                    getInstanceOfSensorSemantic(
+                      Map<String, String>.from(element)),
+                      'NA',
+                      'NA',
+                      'NA'
+                ));
+              });
+            }
+            break;
+
+          case '2': // Magnetometer
+
+            break;
+
+          case '3': // Orientation Sensor
+
+            break;
+
+          case '4': // Gyroscope
+
+            break;
+
+          case '5': // Ambient Light
+        }
+      });
+    } on PlatformException {}
+    setState(() {
+
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _methodChannel = MethodChannel(_methodChannelName);
+    _eventChannel = EventChannel(_eventChannelName);
+    getSensorsList();
+    _eventChannel.receiveBroadcastStream().listen(_onData, onError: _onError);
+  }
+
+  void _onData(dynamic event) {
+    Map<String, String> receivedData = Map<String,String>.from(event);
+    switch (receivedData['type']) {
+      case '1': // Accelerometer
+        _listAccelerometer.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.x = sensorFeed[0];
+              item.y = sensorFeed[1];
+              item.z = sensorFeed[2];
+            });
+          }
+        });
+        break;
+
+      case '2': // Magnetic Field
+        _listMagnetometer.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.x = sensorFeed[0];
+              item.y = sensorFeed[1];
+              item.z = sensorFeed[2];
+            });
+          }
+        });
+        break;
+
+      case '3': // OrientationSensor
+        _listOrientationSensor.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.azimuth = sensorFeed[0];
+              item.pitch = sensorFeed[1];
+              item.roll = sensorFeed[2];
+            });
+          }
+        });
+        break;
+
+      case '4': // Gyroscope
+        _listGyroscope.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.x = sensorFeed[0];
+              item.y = sensorFeed[1];
+              item.z = sensorFeed[2];
+            });
+          }
+        });
+        break;
+
+      case '5': // Ambient Light
+        _listAmbientLight.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.level = sensorFeed[0];
+            });
+          }
+        });
+        break;
+
+      case '6': // Atmospherique Pressure
+        _listAtmosphericPressure.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.pressure = sensorFeed[0];
+            });
+          }
+        });
+        break;
+
+      case '8': // Proximity Sensor
+        _listProximity.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.distance = sensorFeed[0];
+            });
+          }
+        });
+        break;
+
+      case '9': // Gravity
+        _listGravity.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.x = sensorFeed[0];
+              item.y = sensorFeed[1];
+              item.z = sensorFeed[2];
+            });
+          }
+        });
+        break;
+
+      case '10': // Linear Acceleration
+        _listLinearAcceleration.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.x = sensorFeed[0];
+              item.y = sensorFeed[1];
+              item.z = sensorFeed[2];
+            });
+          }
+        });
+        break;
+
+      case '11': // Rotation Vector
+        _listRotationVector.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.x = sensorFeed[0];
+              item.y = sensorFeed[1];
+              item.z = sensorFeed[2];
+              if (sensorFeed.length == 4) {
+                item.someVal = sensorFeed[3];
+              } else if (sensorFeed.length == 5) {
+                item.estimatedHeadingAccuracy = sensorFeed[4];
+              }
+            });
+          }
+        });
+        break;
+
+      case '12': // Relative Humidity
+        _listRelativeHumidity.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.humidity = sensorFeed[0];
+            });
+          }
+        });
+        break;
+
+      case '13':
+        _listAmbientTemperature.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.temperature = sensorFeed[0];
+            });
+          }
+        });
+        break;
+
+      case '14': // Uncalibrated Magnetic Field Sensor
+        _listUncalibratedMagneticField.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.xUncalibrated = sensorFeed[0];
+              item.yUncalibrated = sensorFeed[1];
+              item.zUncalibrated = sensorFeed[2];
+              item.estimatedXBias = sensorFeed[3];
+              item.estimatedYBias = sensorFeed[4];
+              item.estimatedZBias = sensorFeed[5];
+            });
+          }
+        });
+        break;
+
+      case '15': // GameRotationVector
+        _listGameRotationVector.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.x = sensorFeed[0];
+              item.y = sensorFeed[1];
+              item.z = sensorFeed[2];
+              item.someVal;
+              item.estimatedHeadingAccuracy;
+            });
+          }
+        });
+        break;
+
+      case '16': // UncalibratedGyroscope
+        _listUncalibratedGyroscope.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.angularSpeedAroundX = sensorFeed[0];
+              item.angularSpeedAroundY = sensorFeed[1];
+              item.angularSpeedAroundY = sensorFeed[2];
+              item.estimatedDriftAroundX = sensorFeed[3];
+              item.estimatedDriftAroundY = sensorFeed[4];
+              item.estimatedDriftAroundZ = sensorFeed[5];
+            });
+          }
+        });
+        break;
+
+      case '17':
+        _listSignifiantMotion.forEach((item) {
+          if (isAMatch(item.sensor, receivedData)) {
+            List<String> sensorFeed = receivedData['values'].split(';');
+            setState(() {
+              item.isInMotion = sensorFeed[0];
+            });
+          }
+        });
+        break;
+    }
+  }
+
+  void _onError(dynamic error) {}
+
+  bool isAMatch(SensorSemantic data, Map<String, String> receivedData) {
+    return (data.name == receivedData['name'] && data.vendorName == receivedData['vendorName'] && data.version == receivedData['version']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(
+        title: Text(
+          'CowBehavior',
+          style: TextStyle(color: Colors.black),
+        ), backgroundColor: Colors.lightBlue,
+      ),
+    body: ListView(padding:EdgeInsets.all(6.0)));
+  }
+}
